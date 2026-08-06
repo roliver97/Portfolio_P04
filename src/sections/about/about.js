f9954f30978983ee2da652fd1e123f63a7b3bb81
Romà <ro.oliver.gil@gmail.com>
2026-08-06 19:20:20 +0200

    feat(skills): add new skills and append text labels
    - Expand skills array with new technologies
    - Add span element to display skill names alongside icons
    - Standardize icon styling for visual consistency

--- src/sections/about/about.js
+++ src/sections/about/about.js
@@ -1,61 +1,103 @@
 import './about.css'
 
 export const printAbout = () => {
-  templateAbout();
+  templateAbout()
 }
 
 const templateAbout = () => {
-  const aboutSection = document.createElement("section");
-  aboutSection.id = "about";
-  aboutSection.classList.add('whiteSection');
-  document.body.appendChild(aboutSection);
-  
+  const aboutSection = document.createElement('section')
+  aboutSection.id = 'about'
+  aboutSection.classList.add('whiteSection')
+  document.body.appendChild(aboutSection)
 
   /*SECCIONES DE ABOUT*/
-  const aboutDiv = document.createElement("div");
-  const skillsDiv = document.createElement("div");
-  
-  aboutDiv.className = "aboutDiv";
-  skillsDiv.className = "skillsDiv";
+  const aboutDiv = document.createElement('div')
+  const skillsDiv = document.createElement('div')
 
-  aboutSection.appendChild(aboutDiv);
-  aboutSection.appendChild(skillsDiv);
+  aboutDiv.className = 'aboutDiv'
+  skillsDiv.className = 'skillsDiv'
+
+  aboutSection.appendChild(aboutDiv)
+  aboutSection.appendChild(skillsDiv)
 
   /*CONTENIDO DE CADA SECCIÓN*/
   /* About Div */
-  const aboutTitle = document.createElement("h2");
-  const paragraph = document.createElement("p");
+  const aboutTitle = document.createElement('h2')
+  const paragraph = document.createElement('p')
 
-  aboutTitle.textContent = "About me";
-  paragraph.textContent = "I’m a web development apprentice, enjoying the process of building websites that are both practical and user-friendly.";
+  aboutTitle.textContent = 'About me'
+  paragraph.textContent =
+    'I’m a web development apprentice, enjoying the process of building websites that are both practical and user-friendly.'
 
-  aboutDiv.appendChild(aboutTitle);
-  aboutDiv.appendChild(paragraph);
+  aboutDiv.appendChild(aboutTitle)
+  aboutDiv.appendChild(paragraph)
 
   /* Skills Div */
-  const skillsTitle = document.createElement("h2");
-  const skillsList = document.createElement("ul");
+  const skillsTitle = document.createElement('h2')
+  const skillsList = document.createElement('ul')
 
-  skillsTitle.textContent = "Skills";
-  skillsList.className = "skillsList";
+  skillsTitle.textContent = 'Skills'
+  skillsList.className = 'skillsList'
 
   // Array de skills
   const skills = [
-    { name: "HTML", img: import.meta.env.BASE_URL + "icons/html.svg" },
-    { name: "CSS", img: import.meta.env.BASE_URL + "icons/css.svg" },
-    { name: "JavaScript", img: import.meta.env.BASE_URL + "icons/js.svg" },
-    { name: "GitHub", img: import.meta.env.BASE_URL + "icons/github.svg" }
-  ];
+    { name: 'HTML5', img: import.meta.env.BASE_URL + 'icons/skills/html.png' },
+    { name: 'CSS3', img: import.meta.env.BASE_URL + 'icons/skills/css.svg' },
+    {
+      name: 'JavaScript (ES6+)',
+      img: import.meta.env.BASE_URL + 'icons/skills/js.svg'
+    },
+    {
+      name: 'React',
+      img: import.meta.env.BASE_URL + 'icons/skills/react.svg'
+    },
 
-  skills.forEach(skill => {
-  const li = document.createElement("li");
-  const img = document.createElement("img");
-  img.src = skill.img;
-  img.alt = skill.name;
-  li.appendChild(img);
-  skillsList.appendChild(li);
-  });
+    {
+      name: 'Node.js',
+      img: import.meta.env.BASE_URL + 'icons/skills/nodejs.svg'
+    },
+    {
+      name: 'Express JS',
+      img: import.meta.env.BASE_URL + 'icons/skills/expressjs.webp'
+    },
+    {
+      name: 'Puppeteer',
+      img: import.meta.env.BASE_URL + 'icons/skills/puppeteer.svg'
+    },
+    {
+      name: 'MongoDB',
+      img: import.meta.env.BASE_URL + 'icons/skills/mongodb.svg'
+    },
+    {
+      name: 'Vite',
+      img: import.meta.env.BASE_URL + 'icons/skills/vitejs.webp'
+    },
+    {
+      name: 'GitHub',
+      img: import.meta.env.BASE_URL + 'icons/skills/github.svg'
+    },
+    {
+      name: 'ChakraUI',
+      img: import.meta.env.BASE_URL + 'icons/skills/chakraui.png'
+    }
+  ]
 
-  skillsDiv.appendChild(skillsTitle);
-  skillsDiv.appendChild(skillsList);
+  skills.forEach((skill) => {
+    const li = document.createElement('li')
+    li.classList.add('skillItem')
+    const img = document.createElement('img')
+    img.src = skill.img
+    img.alt = skill.name
+
+    const span = document.createElement('span')
+    span.textContent = skill.name
+    span.classList.add('skillSpan')
+
+    li.appendChild(img)
+    li.appendChild(span)
+    skillsList.appendChild(li)
+  })
+
+  skillsDiv.appendChild(skillsTitle)
+  skillsDiv.appendChild(skillsList)
 }
